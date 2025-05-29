0eb348e733b443d90aecc81d4f8c973fa8aaa071
Yashvi <yashvi.sharma1204@gmail.com>
2025-05-29 15:51:12 +0530

    Changed layout and style

--- app.py
+++ app.py
@@ -175,6 +175,4 @@ def load_user(username):
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, debug=True)
